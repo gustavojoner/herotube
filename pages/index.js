71f6d59484cac57afd65e06e59dcd7828046d7ce
Gustavo Joner <gustavo.joner@hotmail.com>
2022-11-08 21:17:18 -0300

    atualizacao 2

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-import config from "../config.json"
-import styled from "styled-components"
+import React from "react";
+import config from "../config.json";
+import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-
-    const estilosHomePage = { };
+    const estilosDaHomePage = { };
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
         <>
-            <CSSReset/>
+            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
                 flex: 1,
-                // backgroundColor: "red",
             }}>
-                <Menu />
+                {/* Prop Drilling */}
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
-                <Timeline playlists={config.playlists} />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
             </div>
         </>
-
-    )
+    );
 }
 
 export default HomePage
@@ -34,51 +36,65 @@ const StyledHeader = styled.div`
         height: 80px;
         border-radius: 50%;
     }
-
     .user-info {
-        margin-top: 50px;
         display: flex;
         align-items: center;
         width: 100%;
-        padding: 16px;
+        padding: 16px 32px;
         gap: 16px;
     }
+`;
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
+    /* background-image: url(${config.bg}); */
+    height: 230px;
 `;
 function Header() {
     return (
         <StyledHeader>
-            {/* <img src="banner"/> */}
+            <StyledBanner bg={config.bg} />
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
                 <div>
-                    <h2>{config.name}</h2>
-                    <p>{config.job}</p>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
                 </div>
             </section>
         </StyledHeader>
     )
 }
 
-function Timeline(props) {
-    const playlistsNames = Object.keys(props.playlists);
+function Timeline({ searchValue, ...propriedades }) {
+    const playlistNames = Object.keys(propriedades.playlists);
     return (
         <StyledTimeline>
-            {playlistsNames.map((playlistName) => {
-                const videos = props.playlists[playlistName];
+            {playlistNames.map((playlistName) => {
+                const videos = propriedades.playlists[playlistName];
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
-                                return (
-                                    <a href={video.url}>
-                                        <img src={video.thumb} />
-                                        <span>
-                                            {video.title}
-                                        </span>
-                                    </a>
-                                )
-                            })}
+                            {videos
+                                .filter((video) => {
+                                    const titleNormalized = video.title.toLowerCase();
+                                    const searchValueNormalized = searchValue.toLowerCase();
+                                    return titleNormalized.includes(searchValueNormalized)
+                                })
+                                .map((video) => {
+                                    return (
+                                        <a key={video.url} href={video.url}>
+                                            <img src={video.thumb} />
+                                            <span>
+                                                {video.title}
+                                            </span>
+                                        </a>
+                                    )
+                                })}
                         </div>
                     </section>
                 )
